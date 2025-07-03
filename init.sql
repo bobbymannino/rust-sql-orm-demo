add126318c285d3c07d94e4acc9531c282500e6f
Bob Mannino <manninobobby@icloud.com>
2025-07-03 21:15:05 +0100

    add init sql script

---
+++ init.sql
@@ -0,0 +1,15 @@
+create table person (
+    person_id serial primary key,
+    first_name text,
+    last_name text not null,
+    created_at timestamptz not null default now()
+);
+
+create table post (
+    post_id uuid primary key default gen_random_uuid(),
+    title text not null,
+    slug text not null unique,
+    content text not null,
+    author_id int not null references person(person_id),
+    created_at timestamptz not null default now()
+);
