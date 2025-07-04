6b33aa2b76c3f968212f68c9dc243d4c55ce6f36
Bob Mannino <manninobobby@icloud.com>
2025-07-04 21:07:05 +0100

    create api server

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,19 @@
+use axum::{Router, routing::get};
 use dotenv::dotenv;
 
+use crate::people::get_people;
+
+mod conn;
 mod model;
+mod people;
 mod schema;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     dotenv().ok();
+
+    let app = Router::new().route("/people", get(get_people));
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3005").await.unwrap();
+
+    axum::serve(listener, app).await.unwrap();
 }
