353580baf679ebc243ce8feaae56b736dd6d2995
Bob Mannino <manninobobby@icloud.com>
2025-07-04 21:06:39 +0100

    fix types in model
    use Uuid and DateTime instead

--- src/model.rs
+++ src/model.rs
@@ -1,24 +1,31 @@
-use diesel::{data_types::PgTimestamp, prelude::*};
+use chrono::{DateTime, Utc};
+use diesel::prelude::*;
+use serde::{Deserialize, Serialize};
+use uuid::Uuid;
 
-#[derive(Queryable, Identifiable, Selectable, Debug, PartialEq)]
+#[derive(
+    Serialize, Deserialize, Queryable, Identifiable, Insertable, Selectable, Debug, PartialEq,
+)]
 #[diesel(table_name = crate::schema::person)]
 #[diesel(primary_key(person_id))]
 pub struct Person {
     pub person_id: i32,
-    pub first_name: String,
-    pub last_name: Option<String>,
-    pub created_at: PgTimestamp,
+    pub first_name: Option<String>,
+    pub last_name: String,
+    pub created_at: DateTime<Utc>,
 }
 
-#[derive(Queryable, Identifiable, Selectable, Debug, PartialEq, Associations)]
+#[derive(
+    Serialize, Deserialize, Queryable, Identifiable, Selectable, Debug, PartialEq, Associations,
+)]
 #[diesel(belongs_to(Person, foreign_key = author_id))]
 #[diesel(table_name = crate::schema::post)]
 #[diesel(primary_key(post_id))]
 pub struct Post {
-    pub post_id: String,
+    pub post_id: Uuid,
     pub title: String,
     pub slug: String,
     pub content: String,
     pub author_id: i32,
-    pub created_at: PgTimestamp,
+    pub created_at: DateTime<Utc>,
 }
