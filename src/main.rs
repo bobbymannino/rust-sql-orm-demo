836ae852a6b21d1f69e547367edf39c7c0bfa3cd
Bob Mannino <manninobobby@icloud.com>
2025-07-04 21:40:55 +0100

    get the GET /people working with the db

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,24 @@
-use axum::{Router, routing::get};
-use dotenv::dotenv;
-
-use crate::people::get_people;
-
 mod conn;
 mod model;
 mod people;
 mod schema;
 
+use axum::{Router, routing::get};
+use dotenv::dotenv;
+
+use crate::people::get_people;
+
+// #[derive(Clone)]
+// struct AppState {}
+
 #[tokio::main]
 async fn main() {
     dotenv().ok();
 
-    let app = Router::new().route("/people", get(get_people));
+    // let app_state = AppState {};
+
+    let app = Router::new().route("/people", get(get_people)); // .with_state(app_state);
+
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3005").await.unwrap();
 
     axum::serve(listener, app).await.unwrap();
--- src/people.rs
+++ src/people.rs
@@ -1,7 +1,12 @@
 use axum::Json;
+use axum::http::StatusCode;
+use diesel::prelude::*;
+use diesel::result::Error;
 use serde::{Deserialize, Serialize};
 
+use crate::conn::get_connection;
 use crate::model::Person;
+use crate::schema::person::dsl::person;
 
 #[derive(Deserialize, Serialize)]
 pub struct People {
@@ -9,11 +14,20 @@ pub struct People {
     total_people: i32,
 }
 
-pub async fn get_people() -> Json<People> {
-    let people = People {
-        people: Vec::new(),
-        total_people: 0,
-    };
+pub async fn get_people() -> Result<Json<People>, StatusCode> {
+    let people = get_people_from_db().await;
 
-    Json(people)
+    match people {
+        Ok(people) => Ok(Json(People {
+            total_people: people.len() as i32,
+            people,
+        })),
+        Err(_) => Err(StatusCode::INTERNAL_SERVER_ERROR),
+    }
+}
+
+async fn get_people_from_db() -> Result<Vec<Person>, Error> {
+    let conn = &mut get_connection();
+
+    person.select(Person::as_select()).load(conn)
 }
